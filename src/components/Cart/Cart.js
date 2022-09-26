529cdab099e290d2f58a7f311384829952dec28d
unknown <sweetpalma9@gmail.com>
2022-09-26 14:22:56 +0600

    order summary position sticky

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className='cart'>
              <h3>Order Summary</h3>
              <p><small>Selected items: {cart.length}</small></p>
         </div>
